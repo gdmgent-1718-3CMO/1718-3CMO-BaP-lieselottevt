d9d3cab7e2c078860cd8fd898a2027aaee61984d
lieselottevt <liesvant4@student.arteveldehogeschool.be>
2018-03-20 21:56:50 +0100

    bug fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $(".link").on('click', function(e) {
-        console.log(e.target.classList[4]);
         if (this.hash !== e.target.classList[4]) {
             e.preventDefault();
             var hash = this.hash;
+            console.log('hash', hash);
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function(){
